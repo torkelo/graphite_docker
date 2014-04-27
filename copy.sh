a44af05b5ddb003e74d7364f9eef4ad1c507a47b
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-04-27 18:04:38 +0200

    added storage schemas

---
+++ copy.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cp	./graphite/local_settings.py /opt/graphite/webapp/graphite/local_settings.py
+cp	./graphite/carbon.conf /opt/graphite/conf/carbon.conf
+cp	./graphite/storage-schemas.conf /opt/graphite/conf/storage-schemas.conf
