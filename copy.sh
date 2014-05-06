ea6bcd671191e7f76b33fa4dd4df2c0d59220b54
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-05-07 00:41:33 +0200

    updated copy script

--- copy.sh
+++ copy.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# this script is just to automate some stuff for play.grafana.org
+
 cp	./graphite/local_settings.py /opt/graphite/webapp/graphite/local_settings.py
 cp	./graphite/carbon.conf /opt/graphite/conf/carbon.conf
 cp	./graphite/storage-schemas.conf /opt/graphite/conf/storage-schemas.conf
+
+cp -f ./dashboards/*.json /src/root/app/dashboards
