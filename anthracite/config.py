3e3993e678962605a86c369c8653e15c863d1069
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-09-17 10:26:01 +0200

    updated anthracite config

--- anthracite/config.py
+++ anthracite/config.py
@@ -3,7 +3,7 @@ import os
 listen_host = '0.0.0.0'  # defaults to "all interfaces"
 listen_port = 8081
 opsreport_start = '01/01/2013 12:00:00 AM'
-timezone = "UTC"
+timezone = "Europe/Stockholm"
 es_url = 'http://' + os.environ['ES_PORT_9200_TCP_ADDR'] + ':9200'
 es_index = 'anthracite'
 
